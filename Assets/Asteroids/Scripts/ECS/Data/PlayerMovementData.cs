113602e1d3564e806d04a140104734b23db3f608
Juan Ignacio Cereda <ceredajuanignacio@gmail.com>
2022-02-23 15:33:45 -0300

    Added thrusters fire

--- Assets/Asteroids/Scripts/ECS/Data/PlayerMovementData.cs
+++ Assets/Asteroids/Scripts/ECS/Data/PlayerMovementData.cs
@@ -3,7 +3,9 @@ using Unity.Entities;
 [GenerateAuthoringComponent]
 public struct PlayerMovementData : IComponentData
 {
+    public float ThrustersForce;
     public float RotationSpeed;
     public float InputRotation;  // -1 counterclockwise, 1 clockwise, 0 no rotation
     public bool ThrustersOn;
+    public Entity ThrustersMeshEntity;
 }
--- Assets/Asteroids/Scripts/ECS/Systems/PlayerMovementSystem.cs
+++ Assets/Asteroids/Scripts/ECS/Systems/PlayerMovementSystem.cs
@@ -2,31 +2,54 @@ using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
 
-public class PlayerMovementSystem : SystemBase {
+public class PlayerMovementSystem : SystemBase
+{
+    private BeginInitializationEntityCommandBufferSystem _endSimulationCommandBufferSystem;
+
+    protected override void OnCreate()
+    {
+        _endSimulationCommandBufferSystem = World.GetOrCreateSystem<BeginInitializationEntityCommandBufferSystem>();
+    }
+
     protected override void OnUpdate()
     {
         float deltaTime = Time.DeltaTime;
-        
-        Entities.ForEach((ref Rotation rotation, ref MovementData movementData, in PlayerMovementData playerMovementData) => {
-            
+        var endCommandBuffer = _endSimulationCommandBufferSystem.CreateCommandBuffer().AsParallelWriter();
+
+        Entities.ForEach((Entity entity,
+            int entityInQueryIndex,
+            ref Rotation rotation,
+            ref MovementData movementData,
+            in PlayerMovementData playerMovementData) =>
+        {
             if (playerMovementData.InputRotation != 0f)
             {
                 quaternion oldRotation = rotation.Value;
                 quaternion rotationToAdd = quaternion.EulerXYZ(playerMovementData.RotationSpeed *
-                                                               playerMovementData.InputRotation * deltaTime * math.up());
-                
+                                                               playerMovementData.InputRotation * deltaTime *
+                                                               math.up());
+
                 quaternion targetRotation = math.mul(oldRotation, rotationToAdd);
 
                 rotation.Value = targetRotation;
             }
-            
+
             movementData.Forward = math.forward(rotation.Value);
-            
+
             if (playerMovementData.ThrustersOn)
             {
-                movementData.Direction = movementData.Forward;
+                movementData.Direction =
+                    math.normalizesafe(movementData.Direction +
+                                       (movementData.Forward * playerMovementData.ThrustersForce * deltaTime));
+                endCommandBuffer.RemoveComponent<Disabled>(entityInQueryIndex, playerMovementData.ThrustersMeshEntity);
             }
-
-        }).Schedule();
+            else
+            {
+                endCommandBuffer.AddComponent<Disabled>(entityInQueryIndex, playerMovementData.ThrustersMeshEntity);
+            }
+            
+        }).ScheduleParallel();
+        
+        _endSimulationCommandBufferSystem.AddJobHandleForProducer(Dependency);
     }
 }
